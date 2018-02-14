661d46846ae6ad78a304a8edd2d9efb7d729fc31
Hidde de Vries <hidde@hiddedevries.nl>
2018-02-14 17:06:39 +0100

    Use ninja extension; add footer styling
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- fractal.js
+++ fractal.js
@@ -9,9 +9,11 @@ fractal.set('project.title', 'CoSS Living Style Guide');
 /* Tell Fractal where the components will live */
 fractal.components.set('path', __dirname + '/src/components');
 
-/* Tell Fractal where the documentation pages will live */
+/* Tell Fractal where the docsumentation pages will live */
 fractal.docs.set('path', __dirname + '/src/docs');
 
 /* Tell Fractal we'll use nunjucks templates */
 fractal.components.engine('@frctl/nunjucks'); 
-fractal.components.set('ext', '.nunj'); 
+fractal.components.set('ext', '.jinja'); 
+
+fractal.components.set('default.preview', '@preview');
