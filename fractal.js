5837c806639dbc39bb036f2a4db6ed58f325eadd
Hidde de Vries <hidde@hiddedevries.nl>
2018-02-28 15:39:56 +0100

    Add Gulp for scss/js, rename assets path
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- fractal.js
+++ fractal.js
@@ -16,4 +16,7 @@ fractal.docs.set('path', __dirname + '/src/docs');
 fractal.components.engine('@frctl/nunjucks'); 
 fractal.components.set('ext', '.jinja'); 
 
-fractal.components.set('default.preview', '@preview');fractal.web.set('static.path', __dirname + '/src/static');
+fractal.components.set('default.preview', '@preview');
+
+fractal.web.set('builder.dest', __dirname + '/build');
+fractal.web.set('static.path', __dirname + '/public');
